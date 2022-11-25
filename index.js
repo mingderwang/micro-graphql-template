4e9dc62c18ab9cd4fea1d9f5456e7262fec52576
Ming-der Wang <mingderwang@gmail.com>
2022-11-25 22:55:01 +0800

    add ethers getBlockNumber

--- index.js
+++ index.js
@@ -2,35 +2,19 @@ const cors = require('micro-cors')(); // highlight-line
 const { ApolloServer, gql } = require('apollo-server-micro');
 const { send } = require('micro');
 const axios = require("axios");
-
-const options = {
-  method: 'POST',
-  url: 'https://blockchain-http-rpc.p.rapidapi.com/api/ethereum/mainnet',
-  headers: {
-    'content-type': 'application/json',
-    'X-RapidAPI-Key': '76d5c05f7emsh731556d3b28871bp17b47ajsnd26fa8bee8e7',
-    'X-RapidAPI-Host': 'blockchain-http-rpc.p.rapidapi.com'
-  },
-  data: '{"jsonrpc":"2.0","method":"eth_blockNumber","params":[],"id":83}'
-};
-
-axios.request(options).then(function (response) {
-	console.log(response.data);
-}).catch(function (error) {
-	console.error(error);
-});
+const ethers = require("ethers")
+const defaultProvider = ethers.getDefaultProvider()
 
 const typeDefs = gql`
   type Query {
-    sayHello: String
+    blockNumber: String
   }
 `;
 
 const resolvers = {
   Query: {
-    async sayHello(parent, args, context) {
-      const respone = await axios.request(options)
-      const result = await JSON.stringify(respone.data)
+    async blockNumber(parent, args, context) {
+      const result = await defaultProvider.getBlockNumber()
       return result;
     },
   },
